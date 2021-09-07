c5bc9d68a32e808d23e4c03c4dd4e074aad61c87
moh bareche <mbareche.bic@gmail.com>
2021-09-07 07:27:26 -0400

    table pray month

---
+++ pray-month.js
@@ -0,0 +1,66 @@
+/* MONTH */
+function loadTimePrayMonthMtl(url, table) {
+  const tableHead = table.querySelector("thead");
+  const tableBody = table.querySelector("tbody");
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then(async (data) => {
+      const prieres = await data.results.datetime;
+
+      /* Clear the table */
+      tableHead.innerHTML = "<tr></tr>";
+      tableBody.innerHTML = "";
+
+      /* Populate the headers */
+      let titres = [];
+
+      titres = Object.keys(prieres[0].date);
+      titres.shift();
+      let titres_pray = Object.keys(prieres[0].times);
+      for (let i in titres_pray) {
+        titres.push(titres_pray[i]);
+      }
+
+      for (let titre of titres) {
+        const headerElement = document.createElement("th");
+        headerElement.textContent = titre;
+        tableHead.querySelector("tr").appendChild(headerElement);
+      }
+
+      /* Populate the row */
+      let dates = [];
+
+      for (let i in prieres) {
+        let dates = Object.values(prieres[i].date);
+        dates.shift();
+        let heures = Object.values(prieres[i].times);
+        let donnees = dates.concat(heures);
+
+        const rowElement = document.createElement("tr");
+        rowElement.setAttribute("class", "day");
+        const today = new Date().toLocaleDateString("fr-CA");
+
+        for (let donnee of donnees) {
+          const cellText = document.createElement("td");
+          cellText.textContent = donnee;
+          rowElement.appendChild(cellText);
+          tableBody.appendChild(rowElement);
+        }
+        console.log(donnees);
+        if (donnees[0] === today.toString()) {
+          // document.getElementsByClassName("day")
+          // active.setAttribute("id", "active");
+          
+        }
+      }
+    });
+
+  // Color background today
+}
+
+loadTimePrayMonthMtl(
+  "https://api.pray.zone/v2/times/this_month.json?city=montreal",
+  document.querySelector("table")
+);
--- pray-times.js
+++ pray-times.js
@@ -1,4 +1,5 @@
-function loadTimePrayMtl(url, table) {
+/* TODAY */
+function loadTimePrayTodayMtl(url, table) {
   const tableHead = table.querySelector("thead");
   const tableBody = table.querySelector("tbody");
   fetch(url)
@@ -10,15 +11,13 @@ function loadTimePrayMtl(url, table) {
       const dateGregorian = await data.results.datetime[0].date.gregorian;
       const dateHijri = await data.results.datetime[0].date.hijri;
 
-      console.log(dateGregorian);
       document.getElementById("date-gregorian").innerHTML = dateGregorian;
       document.getElementById(
         "date-hijri"
       ).innerHTML = `${dateHijri}     الموافق`;
+      
       /* Clear the table */
-
       tableHead.innerHTML = "<tr></tr>";
-
       tableBody.innerHTML = "";
 
       /* Populate the headers */
@@ -43,11 +42,13 @@ function loadTimePrayMtl(url, table) {
     });
 }
 
-// loadTimePrayMtl(
-//   "https://api.pray.zone/v2/times/day.json?city=montreal&date=2021-09-08",
-//   document.querySelector("table")
-// );
-loadTimePrayMtl(
+loadTimePrayTodayMtl(
   "https://api.pray.zone/v2/times/today.json?city=montreal",
   document.querySelector("table")
-);
+  );
+  
+  /* DAY */
+  // loadTimePrayDayMtl(
+//   "https://api.pray.zone/v2/times/day.json?city=mecca&date=2021-09-08",
+//   document.querySelector("table")
+// );
